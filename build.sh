a4b09483ea4189fe7219a33dee750354fe7359b5
ugjka <ugis@germanis.lv>
2024-10-19 20:13:23 +0300

    gradient

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 find _site \( -name '*.css' -o -name '*.html' -o -name '*.js' -o -name '*.json' -o -name '*.txt' -o -name '*.xml' -o -name '*.yml' \) -exec gzip --best --keep -f {} \;
-rsync -Pvhr --exclude=shot.jpg --delete _site/ ugjka.net:share/webcam/
+rsync -Pvhr --exclude=shot.jpg --delete _site/ server.internal:share/irc/
